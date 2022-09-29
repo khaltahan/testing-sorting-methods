561774b430008a25dddcab1ee1c12d35edbe2dc6
Khalid <khaltahan@gmail.com>
2022-09-28 20:21:21 -0400

    collections sort

--- src/TestingSortingMethods.java
+++ src/TestingSortingMethods.java
@@ -6,6 +6,37 @@ public class TestingSortingMethods {
         // Call header method
         printHeader();
 
+        int size = 50000;
+
+        // Initialize main array with size 50000
+        Integer[] mainArr = new Integer[size];
+
+        // Fill array with random integers ranging from 0 to 50000
+        for (int i = 0; i < mainArr.length; i++) {
+            mainArr[i] = (int) (Math.random() * size);
+        }
+
+        // Backup array initialized with same size as main array
+        Integer[] backupArr = new Integer[mainArr.length];
+
+        // Copy the main array to the backup array
+        System.arraycopy(mainArr, 0, backupArr, 0, size);
+
+        // Convert main array to ArrayList
+        ArrayList<Integer> newArrList = new ArrayList<Integer>(Arrays.asList(mainArr));
+
+        // Start timer
+        long startTimer = System.nanoTime();
+        // Sort ArrayList
+        Collections.sort(newArrList);
+        // End timer
+        long endTimer = System.nanoTime();
+        // Calculate time
+        long collectionsTimer = endTimer - startTimer;
+
+        // Copy backup array back to main array to make sure we're sorting the same unsorted array every time
+        System.arraycopy(backupArr, 0, mainArr, 0, size);
+
         // Call header method
         printFooter();
 
